63785c3c5c92331e7cce501d5179c873f67144cf
wfx1024 <33012452+wfx1024@users.noreply.github.com>
2023-02-23 09:40:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  url = "http://t.weather.sojson.com/api/weather/city/" + city
   res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  weather = res['data']['forecast'][0]
+  return weather['type']+","+weather['fx']+"-"+weather['fl'], weather['low']+"-"+weather['high'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
